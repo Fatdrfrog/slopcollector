59387ba20f999567b210021609aa9a61618b341f
Samat Kalshabekov <fatdrfrog@gmail.com>
2025-11-26 18:52:38 +0500

    `posthog`: envs remove coooki mode to get first events

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -3,5 +3,4 @@ import posthog from "posthog-js"
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || "", {
   api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
   defaults: "2025-05-24",
-  cookieless_mode: "on_reject",
 });
